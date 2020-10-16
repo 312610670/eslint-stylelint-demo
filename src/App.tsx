79314076a7bc2ac1897aa1998ba18a3e39f936eb
litianzhi <litz01@mingyuanyun.com>
2020-10-16 17:35:24 +0800

    初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+// 建立路由组件
+import Routers from './routers/Routers'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routers></Routers>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routers/Routers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Routers = () => {
+    return (
+        <>
+            路由哈哈哈哈
+        </>
+    )
+}
+
+export default Routers
