8e3b8a273b23b014d2406ac31955efdd83bb02ea
litianzhi <litz01@mingyuanyun.com>
2020-10-19 14:30:15 +0800

    init

---
+++ src/pages/styleLInts/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './index.module.less'
+const StyleCss = () => {
+    return (
+        <div className={styles.content}>
+            css样式测试
+        </div>
+    )
+}
+
+export default StyleCss
